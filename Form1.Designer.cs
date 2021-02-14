0180eba4c9d8c5d0a5aa7191404c2693d7d3165c
ZvalsoraY <yazamail@yandex.ru>
2021-02-14 15:16:11 +0300

    Buttons added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GuessSound
 {
-    partial class Form1
+    partial class fMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnParams = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlay.Location = new System.Drawing.Point(306, 67);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(252, 69);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Game";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnParams
+            // 
+            this.btnParams.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnParams.Location = new System.Drawing.Point(306, 141);
+            this.btnParams.Name = "btnParams";
+            this.btnParams.Size = new System.Drawing.Size(252, 69);
+            this.btnParams.TabIndex = 1;
+            this.btnParams.Text = "Settings";
+            this.btnParams.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(306, 219);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(252, 69);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GuessSound.Properties.Resources.IMG_20190308_155130;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParams);
+            this.Controls.Add(this.btnPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fMain";
+            this.Text = "Guess the melody";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnParams;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GuessSound
 {
-    public partial class Form1 : Form
+    public partial class fMain : Form
     {
-        public Form1()
+        public fMain()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
